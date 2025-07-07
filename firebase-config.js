8d2fdb5b63cb1d58fad97682612ccb8ae20f9a31
Diogo Monteiro <62382311+dimontei@users.noreply.github.com>
2025-07-07 14:40:05 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Replace these values with your actual Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyCyZz4QBeKuYzi_Lu7PYlizk3BusDID9hI",
+  authDomain: "lateirostour.firebaseapp.com",
+  projectId: "lateirostour",
+  storageBucket: "lateirostour.firebasestorage.app",
+  messagingSenderId: "170506443069",
+  appId: "1:170506443069:web:48992862f6ce4957200d54",
+  measurementId: "G-0WZ07TQEF4"
+};
+firebase.initializeApp(firebaseConfig);
